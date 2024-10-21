fe213e207b3f07aa08c691639360f164327f1aad
Matthew Hasan <matthewhasan@Matthews-Air.lan>
2024-10-20 21:01:43 -0500

    moved index.js to main

--- public/index.js
+++ public/index.js
@@ -1,67 +1,67 @@
-import express from "express";
-import bodyParser from "body-parser";
+// import express from "express";
+// import bodyParser from "body-parser";
 
-const app = express();
-const port = 3000;
+// const app = express();
+// const port = 3000;
 
-// const audio = new Audio("sounds/evil-laugh-89423.mp3")
-
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
-
-let purity = true;
-
-let hitList = [];
-
-const data = {
-    cursedName: "",
-    cursedReason: "",
-    hitList: [],
-}
-
-function addToHitList (victimData) {
-    hitList.push(victimData);
-    return hitList;
-}
+// // const audio = new Audio("sounds/evil-laugh-89423.mp3")
 
 
-app.get("/", (req, res) => {
-    res.render("index.ejs");
-})
+// app.use(bodyParser.urlencoded({extended: true}));
+// app.use(express.static("public"));
 
-app.post("/submit", (req, res) => {
-    purity = false;
-    let newData = {}
-    newData.cursedName = req.body["cursed-name"];
-    newData.cursedReason = req.body["cursed-reason"];
-    addToHitList(newData);
-    data.hitList = hitList;
+// let purity = true;
 
-    data.cursedName = req.body["cursed-name"];
-    data.cursedReason = req.body["cursed-reason"];
+// let hitList = [];
+
+// const data = {
+//     cursedName: "",
+//     cursedReason: "",
+//     hitList: [],
+// }
+
+// function addToHitList (victimData) {
+//     hitList.push(victimData);
+//     return hitList;
+// }
+
+
+// app.get("/", (req, res) => {
+//     res.render("index.ejs");
+// })
+
+// app.post("/submit", (req, res) => {
+//     purity = false;
+//     let newData = {}
+//     newData.cursedName = req.body["cursed-name"];
+//     newData.cursedReason = req.body["cursed-reason"];
+//     addToHitList(newData);
+//     data.hitList = hitList;
+
+//     data.cursedName = req.body["cursed-name"];
+//     data.cursedReason = req.body["cursed-reason"];
 
     
-    console.log(purity)
-    res.render("index.ejs", data);
-});
+//     console.log(purity)
+//     res.render("index.ejs", data);
+// });
 
-app.get("/submit", (req, res) => {
-    purity = true;
-    res.redirect("/")
-})
+// app.get("/submit", (req, res) => {
+//     purity = true;
+//     res.redirect("/")
+// })
 
-app.get("/about", (req, res) => {
-    res.render("about.ejs");
-})
+// app.get("/about", (req, res) => {
+//     res.render("about.ejs");
+// })
 
-app.get("/contact", (req, res) => {
-    res.render("contact.ejs");
-})
+// app.get("/contact", (req, res) => {
+//     res.render("contact.ejs");
+// })
 
 
-app.listen(port, (req, res) => {
-    console.log(`Listening on Port ${port}!`)
-});
+// app.listen(port, (req, res) => {
+//     console.log(`Listening on Port ${port}!`)
+// });
 
-export { purity }
+// export { purity }
